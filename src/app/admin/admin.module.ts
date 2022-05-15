2210d001df07e4b06e3db671e70f155c3892748e
Victor Rolack <victorolack@gmail.com>
2022-05-15 18:16:44 -0400

    card de perfil creada y mockeada

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,12 +5,14 @@ import { DashboardRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SharedModule } from '../shared/shared.module';
 import { NavComponent } from './components/nav/nav.component';
+import { ProfileComponent } from './components/dashboard/components/profile/profile.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    NavComponent
+    NavComponent,
+    ProfileComponent
   ],
   imports: [
     CommonModule,
--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -15,7 +15,7 @@ export class DashboardComponent implements OnInit {
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
+          { title: 'profile', cols: 1, rows: 1 },
           { title: 'Card 2', cols: 1, rows: 1 },
           { title: 'Card 3', cols: 1, rows: 1 },
           { title: 'Card 4', cols: 1, rows: 1 }
@@ -23,7 +23,7 @@ export class DashboardComponent implements OnInit {
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
+        { title: 'Profile', cols: 2, rows: 1 },
         { title: 'Card 2', cols: 1, rows: 1 },
         { title: 'Card 3', cols: 1, rows: 2 },
         { title: 'Card 4', cols: 1, rows: 1 }

9145a04752c4a55703cffd2cee07fbb236d02d56
Victor Rolack <victorolack@gmail.com>
2022-05-14 23:12:17 -0400

    fix login guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { LoginGuard } from './auth/guards/login/login.guard';
 import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [

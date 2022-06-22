76be94726a00de3a8e1d2146828db3bbb087725d
Victor Rolack <victorolack@gmail.com>
2022-06-22 19:32:03 -0400

    add prescription form with calls to get type_medicaments

--- src/app/admin/components/prescription-form/prescription-form.component.ts
+++ src/app/admin/components/prescription-form/prescription-form.component.ts
@@ -1,30 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+
+import { Medicament } from 'src/app/core/models/medicament';
+import { Patient } from 'src/app/core/models/patient';
+import { User } from 'src/app/core/models/user';
+import { TypeMedicament } from 'src/app/core/models/typeMedicament';
+
+import { PrescriptionService } from 'src/app/services/prescription.service';
+import { PatientService } from 'src/app/services/patient.service';
+import { TypeMedicamentService } from 'src/app/services/type-medicament.service';
 
 @Component({
   selector: 'app-prescription-form',
   templateUrl: './prescription-form.component.html',
   styleUrls: ['./prescription-form.component.scss']
 })
-export class PrescriptionFormComponent {
+export class PrescriptionFormComponent implements OnInit {
+
+  userStorage: any = localStorage.getItem('user');
+  user!: User;
+  patient!: Patient;
+  typesMedicaments!: TypeMedicament[]
+
+  medicaments: Medicament[] = [
+    {
+      "id": 1,
+      "name": "ibuprofeno nacional",
+      grammage: 400,
+      stock: 100,
+      format_medicament: "Comprimidos",
+      type_medicament_id: 2,
+      laboratory_name: "Pfizer",
+      principal_agent: "ibuprofeno",
+      secondary_agent: "aspirina",
+      caducity_date: "2002-02-20"
+    },
+    {
+      id: 2,
+      name: "paracetamol nacional",
+      grammage: 400,
+      stock: 100,
+      format_medicament: "Comprimidos",
+      type_medicament_id: 2,
+      laboratory_name: "Pfizer",
+      principal_agent: "paracetamol",
+      secondary_agent: "aspirina",
+      caducity_date: "2001-06-20"
+    },
+  ]
 
   form = this.fb.group({
-    company: null,
-    firstName: [null, Validators.required],
-    secondName: [null],
-    lastName: [null, Validators.required],
-    secondLastName: [null, Validators.required],
     patology: [null, Validators.required],
-    address: [null, Validators.required],
-    phone: [null, Validators.required],
-    email: [null, Validators.required],
+    prescription_date: [null, Validators.required],
+    type_medicament_id: [null, Validators.required],
     medicament: [null, Validators.required]
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private prescriptionService: PrescriptionService,
+    private typeMedicamentService: TypeMedicamentService,
+    private activatedRoute: ActivatedRoute,
+    private patientService: PatientService
+  ) {}
 
-  onSubmit(): void {
-    alert('Thanks!');
+  async ngOnInit() {
+    this.user = JSON.parse(this.userStorage);
+    this.typesMedicaments = await this.typeMedicamentService.getTypesMedicaments();
+    console.log(this.typesMedicaments);
+    this.patient = await this.patientService.getPatient(this.activatedRoute.snapshot.params['rut']);
   }
+
+  onSubmit(data: any){
+    console.log(data)
+  }
+
+
 
 }
---
+++ src/app/core/models/medicament.ts
@@ -0,0 +1,12 @@
+export interface Medicament {
+  id: number;
+  name: string;
+  grammage: number;
+  stock: number;
+  format_medicament: string;
+  type_medicament_id: number;
+  laboratory_name: string;
+  principal_agent: string;
+  secondary_agent: string;
+  caducity_date: string;
+}

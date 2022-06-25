f4ec440dcf0dcca37e52ba631cee0f7fa65775ad
Victor Rolack <victorolack@gmail.com>
2022-06-25 04:20:10 -0400

    front/medi-date-section

---
+++ src/app/admin/components/dashboard/components/medic-date-section/medic-date-section.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+import { MedicDatesService } from 'src/app/services/medic-dates.service';
+
+import { MedicDate } from 'src/app/core/models/medic-date';
+
+
+@Component({
+  selector: 'app-medic-date-section',
+  templateUrl: './medic-date-section.component.html',
+  styleUrls: ['./medic-date-section.component.scss']
+})
+export class MedicDateSectionComponent implements OnInit {
+
+  medicDates!: MedicDate[];
+
+  constructor(
+    private medicDateService: MedicDatesService
+  ) { }
+
+  ngOnInit() {
+    this.getMedicDates();
+    console.log(this.medicDates);
+  }
+
+  async getMedicDates() {
+    this.medicDates = await this.medicDateService.getMedicDates();
+  }
+
+  async getPatients() {
+
+  }
+
+  async deleteMedicDate(medic_date_id: number) {
+    await this.medicDateService.deleteMedicDate(medic_date_id);
+  }
+
+}
